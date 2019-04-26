5634e8a6b97783012c3eb57278d7ebff8e5ef1a7
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-04-25 21:55:24 -0400

    added stuff

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+var app = express();
+app.use(logger('dev'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
+var _ = require("underscore");
