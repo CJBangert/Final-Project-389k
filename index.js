b62558249cb566bfa6ddf65d9bcbe4822008d3e7
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-04-25 22:05:06 -0400

    added home

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
 var _ = require("underscore");
+
+app.get('/',function(req,res){
+	 console.log("Going home...")
+    res.render('home');
+    console.log("home rendered")
+    
+  //  res.send("UNIMPLEMENTED ENDPOINT");
+});	
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
+});
