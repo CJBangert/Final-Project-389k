a50cc493de4913f0d5864d51aae551d2d320f624
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-05-02 23:24:15 -0400

    login api integrated

---
+++ server.js
@@ -0,0 +1,42 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+var logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+var exphbs = require('express-handlebars');
+app.use(logger('dev'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
+var _ = require("underscore");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+app.get('/users/register',function(req,res){
+	console.log("Going home...")
+    res.render('home');
+    console.log("home rendered")
+    
+  //  res.send("UNIMPLEMENTED ENDPOINT");
+});	
+
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
